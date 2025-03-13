bbd18f147164a49cd3d2da415d4f5a5b2ef566ac
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-03-13 16:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', () => {
+    const status = document.getElementById('status');
+    if (status.textContent === 'Enter the Metaverse') {
+        status.innerHTML = '<h1>Entered Metaverse</h1>';
+    }
+})
